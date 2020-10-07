d2faf0f3670a0e3a6d2fab34dc04a37bed1275c6
alekhya87 <davulurialekhya87909.com>
2020-10-07 20:17:54 +0530

    Usecase1 is done

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,11 @@
 #!/bin/bash/
 echo "flip Coin Combination"
-
+#usecase-1
+isHead=1
+randomCheck=$((RANDOM%2))
+if [ $isHead -eq $randomCheck ]
+then
+	echo "Head is Winner"
+else
+	echo "Tail is Winner"
+fi
