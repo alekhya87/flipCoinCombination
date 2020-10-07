18486b41670c8a8a899d15dc343935253c7f5d49
alekhya87 <davulurialekhya87909.com>
2020-10-07 20:40:40 +0530

    usecase3 is done

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -37,3 +37,39 @@ perhead=`expr $perhead / 25`;
 pertail=`expr 100 - $perhead`;
 echo "percentage of heads is $perhead"
 echo "percentage of tails is $pertail"
+#usecase-3
+declare  -A  coin;
+
+coin[headhead]=0;
+coin[tailtail]=0;
+coin[headtail]=0;
+count=0;
+
+addtoDictionary(){
+if [ $1 -eq 0 ]
+then
+coin[headhead]=`expr ${coin[headhead]} + 1`;
+elif [ $1 -eq 1 ]
+then
+coin[tailtail]=`expr ${coin[tailtail]} + 1`;
+else
+coin[headtail]=`expr ${coin[headtail]} + 1`;
+fi
+}
+
+while [ $count -le 25 ]
+do
+flip=$((RANDOM%3));
+addtoDictionary $flip;
+((++count))
+done
+
+perhead=`expr ${coin[headhead]} \* 100`;
+perhead=`expr $perhead / 25`;
+pertail=`expr ${coin[tailtail]} \* 100`;
+pertail=`expr $pertail / 25`;
+perheadtail=`expr ${coin[headtail]} \* 100`;
+perheadtail=`expr $perheadtail / 25`;
+echo "percentage of headhead is $perhead"
+echo "percentage of tailtail is $pertail"
+echo "percentage of headtail is $perheadtail"
