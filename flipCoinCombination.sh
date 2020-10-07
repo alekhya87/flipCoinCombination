02901010c354466158b2e9939e3da0edcb0609c1
alekhya87 <davulurialekhya87909.com>
2020-10-07 20:23:19 +0530

    usecase2 is done

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -9,3 +9,31 @@ then
 else
 	echo "Tail is Winner"
 fi
+#usecase-2
+declare  -A  coin;
+
+coin[head]=0;
+coin[tail]=0;
+count=0;
+
+addtoDictionary(){
+if [ $1 -eq 0 ]
+then
+coin[head]=`expr ${coin[head]} + 1`;
+else
+coin[tail]=`expr ${coin[tail]} + 1`;
+fi
+}
+
+while [ $count -le 25 ]
+do
+flip=$((RANDOM%2));
+addtoDictionary $flip;
+((++count))
+done
+
+perhead=`expr ${coin[head]} \* 100`;
+perhead=`expr $perhead / 25`;
+pertail=`expr 100 - $perhead`;
+echo "percentage of heads is $perhead"
+echo "percentage of tails is $pertail"
